274250aead58207e7d83448563603d6933b7e0d8
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-05-30 16:25:42 +0200

    open api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with('technologies', 'type')->orderByDesc('id')->paginate(12);
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+
+        ]);
+    }
+}
